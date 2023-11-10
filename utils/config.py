5445376a94423226e3b89d6373bed3ad2559a824
DJ Stomp <85457381+DJStompZone@users.noreply.github.com>
2023-11-10 10:02:42 -0800

    Update config.py
    Fixed issue where config file could not be written if forgeyt appdata folder wasn't already present on local filesystem

--- utils/config.py
+++ utils/config.py
@@ -6,11 +6,14 @@ currentversion = "2.2.1"
 prompt = input
 config_folder = path.join(appdata_path, "ForgeYT")
 config_file = path.join(config_folder, "config.json")
+DEFAULT_SETTINGS = {"theme": "system", "download_path": "./video_downloads"}
+
 def makeconfig():
     if not path.exists(config_folder):
         makedirs(config_folder)
-DEFAULT_SETTINGS = {"theme": "system", "download_path": "./video_downloads"}
+
 def load_config():
+    makeconfig()
     if not path.exists(config_file):
         return DEFAULT_SETTINGS
     with open(config_file, "r") as f:
@@ -19,11 +22,10 @@ def load_config():
         except JSONDecodeError:
             # If there's an error, return a default configuration
             return DEFAULT_SETTINGS
+
 config_data = load_config()
 download_path = config_data["download_path"]
 windowTheme = config_data["theme"]
-
-
 
 def resource_path(relative_path):
     try:
