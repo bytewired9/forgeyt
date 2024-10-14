f2fc9a00bcfc0a1727ccbf175ab49cd42380546c
ByteWired9 <77468737+bytewired9@users.noreply.github.com>
2024-10-14 09:06:16 -0500

    remove old dir when done

--- build.py
+++ build.py
@@ -56,6 +56,7 @@ def extract_ffmpeg(archive_path, extract_to):
     if not extract_with_explorer(archive_path, extract_to):
         print("Falling back to 7-Zip extraction method...")
         extract_with_7zip(archive_path, extract_to)
+    os.remove(archive_path)
 
 
 # Step 6: Move ffmpeg.exe and ffprobe.exe to ./ffmpeg and delete the extracted folder
